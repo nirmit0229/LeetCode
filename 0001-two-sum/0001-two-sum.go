d7934f0c65d2a9ed181792a86cbc1b80da4790fe
nirmit0229 <74655326+nirmit0229@users.noreply.github.com>
2024-05-26 11:17:28 +0530

    Time: 7 ms (41.46%), Space: 4.3 MB (12.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	indexMap := make(map[int]int)
+	for currIndex, currNum := range nums {
+		if requiredIdx, isPresent := indexMap[target-currNum]; isPresent {
+			return []int{requiredIdx, currIndex}
+		}
+		indexMap[currNum] = currIndex
+	}
+	return []int{}
+}
